c86b20b7d73027b73f2b661b9bf8fdb7b1776a7f
Sandeep <sandeep@Sandeeps-MacBook-Pro.local>
2025-12-17 20:16:13 +0530

    api corrected

--- frontend/src/store/api.ts
+++ frontend/src/store/api.ts
@@ -2,14 +2,23 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api";
+  process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8000";
 
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: BASE_URL,
+    baseUrl: `${BASE_URL}/api`,
     credentials: "include",
   }),
-  tagTypes: ["User", "Cart", "Order", "Product", "Address", "Color", "Brand", "Category"],
+  tagTypes: [
+    "User",
+    "Cart",
+    "Order",
+    "Product",
+    "Address",
+    "Color",
+    "Brand",
+    "Category",
+  ],
   endpoints: () => ({}),
 });
